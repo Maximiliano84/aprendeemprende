477fdb348fdb70391095850363cd6b2350572d55
maxi cassino <maxi_cassino@hotmail.com>
2023-10-06 13:33:33 -0300

    modificaciones

--- index.js
+++ index.js
@@ -24,28 +24,70 @@ document.addEventListener('DOMContentLoaded', function() {
     fechaFinal.setHours(23, 59, 59, 999); // Establece la hora final a las 23:59:59 del mismo día
     const contadorElemento = document.getElementById('contador');
 
+    let horasRestantes = 0;
+    let minutosRestantes = 0;
+    let segundosRestantes = 0;
+    let contadorActualizado = false;
+
     function actualizarContador() {
         const ahora = new Date();
         const diferencia = fechaFinal - ahora;
 
         if (diferencia <= 0) {
-            contadorElemento.textContent = 'Fin de la oferta :(';
+            if (!contadorActualizado) {
+                contadorElemento.textContent = 'Fin de la oferta :(';
+                console.log('La oferta ha terminado.');
+                contadorActualizado = true;
+            }
         } else {
-            const tiempoRestante = new Date(fechaFinal - ahora);
-            const horas = tiempoRestante.getUTCHours();
-            const minutos = tiempoRestante.getUTCMinutes();
-            const segundos = tiempoRestante.getUTCSeconds();
-
-            contadorElemento.textContent = `${horas.toString().padStart(2, '0')}:${minutos
-                .toString()
-                .padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
+            const tiempoRestante = new Date(diferencia);
+            horasRestantes = tiempoRestante.getUTCHours();
+            minutosRestantes = tiempoRestante.getUTCMinutes();
+            segundosRestantes = tiempoRestante.getUTCSeconds();
+            contadorActualizado = false;
+            console.log(`Tiempo restante: ${horasRestantes}:${minutosRestantes}:${segundosRestantes}`);
         }
     }
 
+    function actualizarElementoContador() {
+        contadorElemento.textContent = `${horasRestantes.toString().padStart(2, '0')}:${minutosRestantes.toString().padStart(2, '0')}:${segundosRestantes.toString().padStart(2, '0')}`;
+        console.log('Contador actualizado en el DOM.');
+    }
+
     // Actualiza el contador cada segundo
-    setInterval(actualizarContador, 1000);
+    setInterval(() => {
+        actualizarContador();
+        if (!contadorActualizado) {
+            actualizarElementoContador();
+        }
+    }, 1000);
 
     // Inicializa el contador
     actualizarContador();
+    actualizarElementoContador();
 
+});
+
+// JavaScript para abrir y cerrar el modal
+const modal = document.querySelector('.modal');
+const modalImage = document.getElementById('modal-image');
+const galleryImages = document.querySelectorAll('.gallery img');
+const closeModal = document.querySelector('.close-modal');
+
+galleryImages.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImage.src = img.src;
+        modalImage.alt = img.alt;
+    });
+});
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
 });
